91e37c27cc384205b7b55f9402f5ee5619fe5da1
DiegoLovera <36526771+DiegoLovera@users.noreply.github.com>
2018-03-18 14:09:22 -0500

    First test

--- app/src/main/java/com/lovera/diego/restock/LoginActivity.java
+++ app/src/main/java/com/lovera/diego/restock/LoginActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
 
+    public static String MAIN_TEST;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
