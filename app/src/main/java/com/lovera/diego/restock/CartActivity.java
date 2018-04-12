89d312296f00c8ec153ab2f8b64090ec696d27ad
flopez <federicopellicer19@gmail.com>
2018-04-12 01:27:07 -0500

    Back button en CartActivity

--- app/src/main/java/com/lovera/diego/restock/CartActivity.java
+++ app/src/main/java/com/lovera/diego/restock/CartActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -24,6 +25,8 @@ public class CartActivity extends AppCompatActivity implements CartItemAdapter.I
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
 
         mRecyclerView = findViewById(R.id.recycler_view_cart_activity);
         mRecyclerView.setHasFixedSize(true);
@@ -50,6 +53,16 @@ public class CartActivity extends AppCompatActivity implements CartItemAdapter.I
         });
     }
     //endregion
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id==android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     //region onCartItemClick
     @Override
     public void onCartItemClick(View view, int position) {
